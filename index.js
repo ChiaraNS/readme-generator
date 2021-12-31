1bd3f818736817b80e8ec9858bbc0aeece6cc982
Key <cns3.22202@gmail.com>
2021-12-31 01:57:07 -0600

    prompt questions

--- index.js
+++ index.js
@@ -4,7 +4,77 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?',
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the project title?',
+        },
+        {
+            type:'input',
+            name: 'description',
+            message: 'Write a descritption of your project',
+        },
+        {
+            type: 'checkbox',
+            name: 'technology',
+            message: 'What languages were used in the creating of this porject?',
+            choices: [
+                'HTML',
+                'CSS',
+                'JavaScript',
+                'React',
+                'Python',
+                'Other',
+            ]
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Enter the installation process',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Enter project usage information',
+        },
+        {
+            type: 'input',
+            name: 'cotributing',
+            message: 'Enter contributers to this project',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Enter project test information'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Chose a license for this project',
+            choices: [
+                'Apache',
+                'GNU',
+                'ISC',
+                'MIT',
+                'Mozilla',
+                'other',
+                'none',
+            ]
+        },
+    ])
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
